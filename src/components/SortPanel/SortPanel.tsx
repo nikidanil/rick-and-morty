e9b18bad91a7947e66ac0671241589d06b80faf8
Nikita <nekit9401@yandex.ru>
2025-12-09 12:04:35 +0300

    feat: implemented SortPanel component

---
+++ src/components/SortPanel/SortPanel.tsx
@@ -0,0 +1,30 @@
+import style from './SortPanel.module.css';
+
+type SortOrder = 'asc' | 'desc';
+
+type SortPanelProps = {
+	label: string;
+	sortOrder: SortOrder;
+	setSortOrder: (sortOrder: SortOrder) => void;
+};
+
+export const SortPanel = ({ label, sortOrder, setSortOrder }: SortPanelProps) => {
+	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		const value = e.target.value;
+		if (value === 'asc' || value === 'desc') {
+			setSortOrder(value);
+		}
+	};
+
+	return (
+		<div className={style.controls}>
+			<label>
+				{label}: &nbsp;
+				<select value={sortOrder} onChange={handleChange} className={style.select}>
+					<option value='desc'>Сначала новые</option>
+					<option value='asc'>Сначала старые</option>
+				</select>
+			</label>
+		</div>
+	);
+};
