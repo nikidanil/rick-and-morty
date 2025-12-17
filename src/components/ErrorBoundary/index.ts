f649ba183a49f40061dbfef2061b9df403f2cd03
Nikita <nekit9401@yandex.ru>
2025-12-17 09:31:26 +0300

    feat: implemented ErrorBoundary

--- src/components/ErrorBoundary/index.ts
+++ src/components/ErrorBoundary/index.ts
@@ -1,2 +1,1 @@
-export * from './SortPanel';
 export * from './ErrorBoundary';
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -2,15 +2,18 @@ import { Outlet } from 'react-router-dom';
 import { Header } from './components';
 import style from './MainLayout.module.css';
 import { Suspense } from 'react';
+import { ErrorBoundary } from '../../components';
 
 export const MainLayout = () => {
 	return (
 		<>
 			<Header />
 			<main className={style.container}>
-				<Suspense fallback={<div>Загрузка...</div>}>
-					<Outlet />
-				</Suspense>
+				<ErrorBoundary fallback={<p>Резервный интерфейс при ошибке.</p>}>
+					<Suspense fallback={<div>Загрузка...</div>}>
+						<Outlet />
+					</Suspense>
+				</ErrorBoundary>
 			</main>
 		</>
 	);
